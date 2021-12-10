0ad17ef424bde4c8044811c97fe9f2cdcc80c4db
“Breno <brenorodriguesdev@gmail.com>
2021-12-09 23:51:14 -0300

    feat: add produto page

--- src/presentation/pages/produto.tsx
+++ src/presentation/pages/produto.tsx
@@ -1,7 +1,7 @@
 import SideBarComponent from '../components/side-bar'
 
 
-export default function HomePage() {
+export default function ProdutoPage() {
   return (
     <>
       <div>
@@ -11,7 +11,7 @@ export default function HomePage() {
           <main className="flex-1">
             <div className="py-6">
               <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-                <h1 className="text-2xl font-semibold text-gray-900">Página Inicial</h1>
+                <h1 className="text-2xl font-semibold text-gray-900">Produto</h1>
               </div>
               <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                 {/* Replace with your content */}
