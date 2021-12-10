15798b7d67a85d307dbd662517d09fa7aa70495f
“Breno <brenorodriguesdev@gmail.com>
2021-12-10 01:05:00 -0300

    feat: inject produto page factory

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import { makeProdutoPage } from "./main/factories/pages/produto";
 import EstoquePage from "./presentation/pages/estoque";
 import HomePage from './presentation/pages/home';
-import ProdutoPage from "./presentation/pages/produto";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/produto" element={<ProdutoPage />} />
+        <Route path="/produto" element={makeProdutoPage()} />
         <Route path="/estoque" element={<EstoquePage />} />
       </Routes>
     </BrowserRouter>
