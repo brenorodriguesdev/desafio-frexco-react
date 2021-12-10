73922de7af8c6905d679b1ac26d9eb1d6b1354d6
“Breno <brenorodriguesdev@gmail.com>
2021-12-10 01:11:57 -0300

    feat: call table with estoques

--- src/presentation/pages/estoque.tsx
+++ src/presentation/pages/estoque.tsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from 'react'
+import { EstoqueModel } from '../../domain/models/estoque/estoque'
+import { ListarEstoquesUseCase } from '../../domain/useCases/estoque/listar-estoques'
 import SideBarComponent from '../components/side-bar'
+import TableComponent from '../components/table'
 
+interface EstoquePageProps {
+  listarEstoquesUseCase: ListarEstoquesUseCase
+}
 
-export default function EstoquePage() {
+export default function EstoquePage({ listarEstoquesUseCase }: EstoquePageProps) {
+
+  const [estoques, setEstoques] = useState<EstoqueModel[]>([])
+
+  useEffect(() => {
+    async function listarProdutos() {
+      try {
+        const estoques = await listarEstoquesUseCase.listar()
+        setEstoques(estoques)
+      } catch (error) {
+        setEstoques([])
+      }
+    }
+
+    listarProdutos()
+  }, [])
+
   return (
     <>
       <div>
         <SideBarComponent />
-        
+
         <div className="md:pl-64 flex flex-col flex-1">
           <main className="flex-1">
             <div className="py-6">
@@ -15,15 +38,18 @@ export default function EstoquePage() {
               </div>
               <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                 {/* Replace with your content */}
-                <div className="py-4">
-                  <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+                <div className="py-16">
+                  <TableComponent items={estoques} columns={[
+                    'id',
+                    'nome'
+                  ]} />
                 </div>
                 {/* /End replace */}
               </div>
             </div>
           </main>
         </div>
-        
+
       </div>
     </>
   )
