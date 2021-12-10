85d912e873cb149be84ca8a36919bd0a59a9634d
“Breno <brenorodriguesdev@gmail.com>
2021-12-09 23:51:33 -0300

    feat: add router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import HomePage from "./presentation/pages/home"
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import EstoquePage from "./presentation/pages/estoque";
+import HomePage from './presentation/pages/home';
+import ProdutoPage from "./presentation/pages/produto";
 
 export default function App() {
   return (
-    <HomePage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/produto" element={<ProdutoPage />} />
+        <Route path="/estoque" element={<EstoquePage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
