d2830dbcd1836e9c39af492d36431bb2dfd804d1
“Breno <brenorodriguesdev@gmail.com>
2021-12-10 02:25:08 -0300

    feat: add deletar estoque service

---
+++ src/data/services/estoque/deletar-estoque.ts
@@ -0,0 +1,12 @@
+import { DeletarEstoqueUseCase } from "../../../domain/useCases/estoque/deletar-estoque";
+import { HttpClient } from "../../contracts/http-client";
+
+export class DeletarEstoqueService implements DeletarEstoqueUseCase {
+    constructor (private readonly httpClient: HttpClient) {}
+    async deletar(id: number): Promise<void | Error> {
+        const result = await this.httpClient.delete({ id })
+        if (result instanceof Error) {
+            throw result
+        }
+    }
+}
