4be4d0aa3dca3054364f6d5bf2105f5dfe58c750
“Breno <brenorodriguesdev@gmail.com>
2021-12-10 00:23:13 -0300

    feat: add listarProdutosUseCase on produto page

--- src/presentation/pages/produto.tsx
+++ src/presentation/pages/produto.tsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from 'react'
+import { ProdutoModel } from '../../domain/models/estoque/produto'
+import { ListarProdutosUseCase } from '../../domain/useCases/produto/listar-produtos'
 import SideBarComponent from '../components/side-bar'
 import TableComponent from '../components/table'
 
+interface ProdutoPageProps {
+  listarProdutosUseCase: ListarProdutosUseCase
+}
 
-export default function ProdutoPage() {
+export default function ProdutoPage({ listarProdutosUseCase }: ProdutoPageProps) {
+
+  const [produtos, setProdutos] = useState<ProdutoModel[]>()
+
+  useEffect(() => {
+    async function listarProdutos() {
+      try {
+        const produtos = await listarProdutosUseCase.listar()
+        setProdutos(produtos)
+      } catch (error) {
+        setProdutos([])
+      }
+    }
+
+    listarProdutos()
+  }, [])
+
   return (
     <>
       <div>
         <SideBarComponent />
-        
+
         <div className="md:pl-64 flex flex-col flex-1">
           <main className="flex-1">
             <div className="py-6">
@@ -17,14 +39,14 @@ export default function ProdutoPage() {
               <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                 {/* Replace with your content */}
                 <div className="py-16">
-                  <TableComponent />
+                  <TableComponent items={produtos} />
                 </div>
                 {/* /End replace */}
               </div>
             </div>
           </main>
         </div>
-        
+
       </div>
     </>
   )
