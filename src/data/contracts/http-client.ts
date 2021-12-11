6184b43bb372e3fdd65d319701b1e8760122f094
“Breno <brenorodriguesdev@gmail.com>
2021-12-10 23:01:47 -0300

    feat: add getByRoute contract on httpClient

--- src/data/contracts/http-client.ts
+++ src/data/contracts/http-client.ts
@@ -2,5 +2,6 @@ export interface HttpClient {
     post(body: any): Promise<any>
     put(body: any): Promise<any>
     get(params: any): Promise<any>
+    getByRoute(route: string): Promise<any>
     delete(params: any): Promise<any>
 }
