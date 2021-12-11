dfe54da8a152a79d99bb75aa2e87762bb95baa38
“Breno <brenorodriguesdev@gmail.com>
2021-12-10 22:46:42 -0300

    feat: add produto page factory

--- src/main/factories/pages/produto.tsx
+++ src/main/factories/pages/produto.tsx
@@ -1,8 +1,12 @@
+import { ProdutoProvider } from "../../../presentation/contexts/produto";
 import ProdutoPage from "../../../presentation/pages/produto";
+import { makeDeletarProdutoService } from "../services/produto/deletar-produto";
 import { makeListarProdutosService } from "../services/produto/listar-produtos";
 
 export function makeProdutoPage() {
     return (
-        <ProdutoPage listarProdutosUseCase={makeListarProdutosService()} />
+        <ProdutoProvider>
+            <ProdutoPage listarProdutosUseCase={makeListarProdutosService()} deletarProdutoUseCase={makeDeletarProdutoService()} />
+        </ProdutoProvider>
     )
 }
