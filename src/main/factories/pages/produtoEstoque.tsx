c08b00762e01ac11dbd2e02c466702eebabe0aa0
“Breno <brenorodriguesdev@gmail.com>
2021-12-14 06:14:04 -0300

    fix: produto estoque page factory

--- src/main/factories/pages/produtoEstoque.tsx
+++ src/main/factories/pages/produtoEstoque.tsx
@@ -1,12 +1,12 @@
 import { ProdutoEstoqueProvider } from "../../../presentation/contexts/produtoEstoque";
 import { makeDeletarProdutoService } from "../services/produto/deletar-produto";
-import { makeListarProdutosService } from "../services/produto/listar-produtos";
 import ProdutoEstoquePage from "../../../presentation/pages/produtoEstoque";
+import { makeListarEstoquesService } from "../services/estoque/listar-estoques";
 
 export function makeProdutoEstoquePage() {
     return (
         <ProdutoEstoqueProvider>
-            <ProdutoEstoquePage listarProdutosUseCase={makeListarProdutosService()} deletarProdutoUseCase={makeDeletarProdutoService()} />
+            <ProdutoEstoquePage listarEstoquesUseCase={makeListarEstoquesService()} deletarProdutoUseCase={makeDeletarProdutoService()} />
         </ProdutoEstoqueProvider>
     )
 }
